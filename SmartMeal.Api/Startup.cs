7434e645951de672ddb550beaea837f9592fa6ef
Dariusz <dariusztomaszkowalczyk@gmail.com>
2019-03-17 15:11:46 +0100

    Added dbcontext with dbcontextfactory

--- SmartMeal.Api/Startup.cs
+++ SmartMeal.Api/Startup.cs
@@ -1,22 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using SmartMeal.Data.Data;
+using System.Reflection;
 
-namespace SmartMeal
+namespace SmartMeal.Api
 {
     public class Startup
     {
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            DotNetEnv.Env.Load("../.env");
+        }
+
+        private string GetConnectionString()
+        {
+            string host = DotNetEnv.Env.GetString("POSTGRES_HOST");
+            string port = DotNetEnv.Env.GetString("POSTGRES_PORT");
+            string db = DotNetEnv.Env.GetString("POSTGRES_DB");
+            string user = DotNetEnv.Env.GetString("POSTGRES_USER");
+            string password = DotNetEnv.Env.GetString("POSTGRES_PASSWORD");
+            string connectionString = $"host={host};port={port};database={db};username={user};password={password};";
+
+            return connectionString;
         }
 
         public IConfiguration Configuration { get; }
@@ -24,6 +34,10 @@ namespace SmartMeal
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connectionString = GetConnectionString();
+            string migrationAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+            services.AddEntityFrameworkNpgsql().AddDbContext<SmartMealContext>(options => 
+                options.UseNpgsql(connectionString, b => b.MigrationsAssembly(migrationAssembly)));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
---
+++ SmartMeal.Data/Data/SmartMealContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SmartMeal.Data.Data
+{
+    public class SmartMealContext : DbContext
+    {
+        public SmartMealContext(DbContextOptions<SmartMealContext> options) : base(options)
+        { 
+        }
+    }
+}
