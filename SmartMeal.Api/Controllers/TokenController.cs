3ee198329c6e061c0869bc93c5fb75c629ed379f
Dariusz <dariusztomaszkowalczyk@gmail.com>
2019-05-02 19:25:58 +0200

    Added Authentication Endpoint

---
+++ SmartMeal.Api/Controllers/TokenController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features.Authentication;
+using Microsoft.AspNetCore.Identity.UI.V3.Pages.Account.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SmartMeal.Models.ModelsDto;
+using SmartMeal.Service.Interfaces;
+
+namespace SmartMeal.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        
+        private readonly ITokenService _tokenService;
+
+        public TokenController(ITokenService tokenService)
+        {
+            _tokenService = tokenService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> CreateToken([FromBody] LoginDto login)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState.Values.SelectMany(x => x.Errors.Select(y => y.ErrorMessage)));
+            }
+
+            IActionResult response = Unauthorized();
+
+            var token = await _tokenService.Authenticate(login);
+
+            if (token != null)
+            {
+                response = Ok(token);
+            }
+
+            return response;
+        }
+    }
+}
---
+++ SmartMeal.Service/Services/TokenService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using SmartMeal.Data.Repository.Interfaces;
+using SmartMeal.Models.Models;
+using SmartMeal.Models.ModelsDto;
+using SmartMeal.Service.Interfaces;
+
+namespace SmartMeal.Service.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly IRepository<User> _repository;
+        private readonly IConfiguration _config;
+
+        public TokenService(IConfiguration config, IRepository<User> repository)
+        {
+            _repository = repository;
+            _config = config;
+        }
+
+        public async Task<AuthDto> Authenticate(LoginDto model)
+        {
+            var user = await _repository.GetByAsync(x => x.Email == model.Email && x.Password == HashManager.GetHash(model.Password));
+
+
+            if (user != null)
+            {
+                var token = BuildToken(user);
+                return new AuthDto()
+                {
+                    Token = token
+                };
+            }
+
+            return null;
+        }
+
+        private string BuildToken(User user)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+                _config["Jwt:Issuer"],
+                claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
