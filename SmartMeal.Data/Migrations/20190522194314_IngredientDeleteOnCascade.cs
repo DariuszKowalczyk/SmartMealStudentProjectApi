2f604875ef11e74c9b7188219dc0cd370c49e9d1
Grzesiek <grzesiek.dlugokecki@gmail.com>
2019-05-23 01:00:28 +0200

    ingredients tests

---
+++ SmartMeal.Data/Migrations/20190522194314_IngredientDeleteOnCascade.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SmartMeal.Data.Migrations
+{
+    public partial class IngredientDeleteOnCascade : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ingredient_Product_ProductId",
+                table: "Ingredient");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ingredient_Product_ProductId",
+                table: "Ingredient",
+                column: "ProductId",
+                principalTable: "Product",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ingredient_Product_ProductId",
+                table: "Ingredient");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ingredient_Product_ProductId",
+                table: "Ingredient",
+                column: "ProductId",
+                principalTable: "Product",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
